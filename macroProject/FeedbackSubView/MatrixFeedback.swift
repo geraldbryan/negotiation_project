563f3fe9a61071900a4a917b0336451fcceed3f0
jason <lauwrin@gmail.com>
2022-10-25 11:04:15 +0700

    Perbaikan UI + matrixFeedback dan confidenceFeedback view

---
+++ macroProject/FeedbackSubView/MatrixFeedback.swift
@@ -0,0 +1,163 @@
+//
+//  MatrixFeedback.swift
+//  macroProject
+//
+//  Created by Jason Lauwrin on 24/10/22.
+//
+
+import SwiftUI
+
+struct MatrixFeedback: View{
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+
+    var btnBackMatrixFeedback : some View { Button(action: {
+        self.presentationMode.wrappedValue.dismiss()
+    }){
+        HStack{
+            Image(systemName: "chevron.left")
+                .font(.system(size: 10, weight: .semibold))
+                .foregroundColor(.white)
+                .frame(width: 25, height: 25)
+                .background(Color("green_tone"))
+                .clipShape(Circle())
+                .frame(alignment: .topLeading)
+            
+            Text("  Matrix")
+                .font(.system(size: 28, weight: .bold))
+                .foregroundColor(Color.black)
+                .frame(alignment: .topLeading)
+            
+            }
+        .frame(width: 360, height: 100, alignment: .leading)
+        }
+    }
+    
+    var body: some View{
+        ScrollView(){
+            ZStack{
+                Color("yellow_tone").ignoresSafeArea()
+                VStack{
+                    Text("Matrix evaluation can help you in designing \na mindset for decision making and style application. The rating will depend on the \nanswer you choose.")
+                        .font(.system(size: 15, weight: .semibold))
+                        .frame(width: 350, height: 80, alignment: .leading)
+                    
+                    Text("Best Style Implementation")
+                        .font(.system(size: 24, weight: .bold))
+                        .frame(width: 350, height: 50, alignment: .leading)
+                        .foregroundColor(Color("green_tone"))
+                        .offset(y: 20)
+                    
+                    HStack{
+                        Image(systemName: "peacesign")
+                            .font(.system(size: 25, weight: .bold))
+                        Text("Compromising")
+                            .font(.system(size: 25, weight: .bold))
+                    }.frame(width: 350, height: 45, alignment: .leading)
+                        .offset(y: 20)
+                        .foregroundColor(Color(.black))
+                    
+                    VStack{
+                        Text("The printing company stated that they willing to speed up the order with extra charge of they wanted. up the order with extra charge of they wanted. ")
+                            .font(.system(size: 15, weight: .light))
+                            .frame(width: 350, height: 80, alignment: .leading)
+                    }.offset(y: 30)
+                    
+                    Text("Answer Evaluation")
+                        .font(.system(size: 24, weight: .bold))
+                        .frame(width: 350, height: 80, alignment: .leading)
+                        .foregroundColor(Color("green_tone"))
+                        .offset(y: 30)
+                    
+                    VStack{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 25)
+                                .foregroundColor(.white)
+                                .shadow(radius: 2)
+                                .frame(width: 250)
+                            VStack{
+                                Text("For you order, the normal production time required is 7 days")
+                                    .font(.system(size: 15, weight: .light))
+                                    .frame(width: 230, height: 60, alignment: .center)
+                            }
+                        }.frame(width: 70,height: 100, alignment: .trailing)
+                            .offset(x: 40, y: 40)
+                        
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 25)
+                                .foregroundColor(Color("green_tone"))
+                                .shadow(radius: 2)
+                                .frame(width: 250)
+                            VStack{
+                                Text("The item will be used at the end of this week. can it be finished in 3 days?")
+                                    .font(.system(size: 15, weight: .light))
+                                    .frame(width: 230, height: 60, alignment: .center)
+                                    .foregroundColor(.white)
+                            }
+                        }.frame(width: 70,height: 100, alignment: .leading)
+                            .offset(x: -40, y: 50)
+                        
+                        VStack{
+                            Text("By explaining the reason of your offer with polite word and expression, you can maintain the relationship")
+                                .font(.system(size: 15, weight: .light))
+                                .frame(width: 350, height: 80, alignment: .leading)
+                        }.offset(y: 60)
+                        Divider().frame(width: 350).offset(y: 60)
+                    }.frame(height: 260)
+                    
+                    VStack{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 25)
+                                .foregroundColor(.white)
+                                .shadow(radius: 2)
+                                .frame(width: 250)
+                            VStack{
+                                Text("It seems difficult to complete in 3 days, the workload will also make it difficult for our employees")
+                                    .font(.system(size: 15, weight: .light))
+                                    .frame(width: 230, height: 60, alignment: .center)
+                            }
+                        }.frame(width: 70,height: 100, alignment: .trailing)
+                            .offset(x: 40, y: 70)
+                        
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 25)
+                                .foregroundColor(Color("green_tone"))
+                                .shadow(radius: 2)
+                                .frame(width: 250)
+                            VStack{
+                                Text("I understand, is there any suggestion to speed up the production?")
+                                    .font(.system(size: 15, weight: .light))
+                                    .frame(width: 230, height: 60, alignment: .center)
+                                    .foregroundColor(.white)
+                            }
+                        }.frame(width: 70,height: 100, alignment: .leading)
+                            .offset(x: -40, y: 80)
+                        
+                        VStack{
+                            Text("The answer you choose is correct. Be sure to explain the reason for the offer you are making and use polite word and expression.")
+                                .font(.system(size: 15, weight: .light))
+                                .frame(width: 350, height: 80, alignment: .leading)
+                        }.offset(y: 90)
+                        Divider().frame(width: 350).offset(y: 90)
+                    }.frame(height: 350)
+                        .offset(y: 10)
+                    
+                    VStack{
+                        Text("")
+                            .font(.system(size: 18, weight: .bold))
+                            .frame(width: 350, height: 200, alignment: .leading)
+                    }
+                }.offset(y: 50)
+            }
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(leading: btnBackMatrixFeedback)
+        }.ignoresSafeArea()
+    }
+}
+
+struct MatrixFeedbackView_Previews: PreviewProvider{
+    static var previews: some View{
+        MatrixFeedback()
+    }
+}
+
+
--- macroProject/View/backgroundview.swift
+++ macroProject/View/backgroundview.swift
@@ -14,30 +14,45 @@ struct backgroundview: View {
                 Color("green_tone").ignoresSafeArea()
                 VStack(spacing:30){
                     ZStack (alignment: .leading){
-                        Image("storyBackground").scaledToFill().frame(width: 300, height: 620).padding(.trailing,8)
+                        RoundedRectangle(cornerRadius: 25)
+                            .foregroundColor(Color("yellow_tone"))
+                            .shadow(radius: 2)
+                            .frame(width: 350, height: 650)
+                        Image("peniti")
+                            .offset(x: 30, y: -312)
+                        RoundedRectangle(cornerRadius: 25)
+                            .foregroundColor(.gray)
+                            .frame(width: 80, height: 5)
+                            .offset(x: 220, y: 300)
                         VStack(alignment: .leading, spacing:14){
                             HStack(spacing:20){
                                 Image("\(storyDetail[0].img!)")
                                     .aspectRatio(contentMode: .fit)
                                     .frame(width:84, height: 79)
                                     .padding(.top,80)
+                                    .offset(x: 10)
                                 Text("\(storyDetail[0].title!)")
                                     .foregroundColor(Color("green_tone"))
                                     .font(.system(size: 32))
                                     .fontWeight(.bold).padding(.top,80).foregroundColor(.black)
+                                    .offset(x: 10)
                             }.frame(width:300, height: 160, alignment: .center)
                             
                             Text("\(storyDetail[0].text!)").frame(width:300).font(.system(size: 16)).padding(.leading,14).foregroundColor(.black)
+                                .offset(x: 10)
                             
                             Text("Details :").font(.system(size: 20))
                                 .fontWeight(.bold).padding(.leading, 14).foregroundColor(.black)
+                                .offset(x: 10)
                             Text("\(storyDetail[0].details!)").frame(width:300).lineSpacing(-10).padding(.top,-10).font(.system(size: 16)).foregroundColor(.black)
-                            
+                                .offset(x: 10)
                             Text("Objectives :").font(.system(size: 20))
                                 .fontWeight(.bold).padding(.leading, 14).foregroundColor(.black)
+                                .offset(x: 10)
                             Text("\(storyDetail[0].objectives!)").frame(width:300).padding(.leading,-18).padding(.top,-10).font(.system(size: 16)).foregroundColor(.black)
-                            Spacer()
+                                .offset(x: 10)
                         }
+                        .offset(y: -50)
                         
                         
                     }
